ebdac080543d1c7e94a58df8d0ac1e2015573cce
kyle <Kyle>
2020-02-10 00:00:06 -0500

    Added updated meta tags

--- cheapchat/lib/header.php
+++ cheapchat/lib/header.php
@@ -7,7 +7,11 @@ include 'functions.php'
 ?>
 <!DOCTYPE html>
 <html>
-<header>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- bootstrap 4 cdn -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<title>CheapChat Project</title>
 	<style>
@@ -35,7 +39,7 @@ include 'functions.php'
 			
 		}
 	</style>
-</header>
+</head>
 <body>
 	<h1 class="text-center text-danger">CheapChat App</h1>
 	<h2 class="text-center">Where you can type whatever....</h2>
