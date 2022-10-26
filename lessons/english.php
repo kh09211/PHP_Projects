6154d5647a9372955adc32b19767769dcb6140b1
Kyle Hopkins <kh09211@georgiasouthern.edu>
2022-10-26 00:25:52 -0500

    Added rew/forward buttons to English/Spanish course scripts

--- lessons/english.php
+++ lessons/english.php
@@ -23,24 +23,37 @@ $filenames = array_values(array_diff(scandir('audios'), array('..', '.')));
 		el.src = 'audios/' + filename;
 		el.play();
 	}
+
+	function rewind15() {
+		var el = document.getElementById('audio');
+		el.currentTime -= 10;
+		el.play();
+	}
+
+	function forward15() {
+		var el = document.getElementById('audio');
+		el.currentTime += 10;
+		el.play();
+	}
+
 </script>
 
 <body style="background-color:azure">
 	<div style="display: flex; flex-direction: row; justify-content: center; padding: 20px;">
-		<div id="player">
-			<audio controls id="audio">
+		<button style="display: block; height: 40px;" onclick="rewind15()">-10sec</button>
+		<div id="player" style="width: 100%;">
+			<audio controls id="audio" style="width: 100%;">
 				<source id="source" src="audios/<?php echo $filenames[0] ?>" type="audio/mpeg">
 				Your browser does not support the audio element.
 			</audio>
 		</div>
+		<button style="display: block; height: 40px;" onclick="forward15()">+10sec</button>
 	</div>
 	<div style="display: flex; flex-direction: row; justify-content: center;  flex-wrap: wrap;">
 		<?php foreach ($filenames as $lesson) { ?>
-			<button
-			style="width: 175px; margin: 3px;"
-			onClick="changeAudio('<?php echo $lesson ?>')"
-			>
-				<?php $name = explode(' ', str_replace('.mp3','',$lesson)); echo 'Eng ' . $name[1] . ' - Lesson - ' . $name[2]; ?>
+			<button style="width: 175px; margin: 3px;" onClick="changeAudio('<?php echo $lesson ?>')">
+				<?php $name = explode(' ', str_replace('.mp3', '', $lesson));
+				echo 'Eng ' . $name[1] . ' - Lesson - ' . $name[2]; ?>
 			</button>
 		<?php } ?>
 	</div>
--- lessons/spanish.php
+++ lessons/spanish.php
@@ -23,27 +23,42 @@ $filenames = array_values(array_diff(scandir('audios_spanish'), array('..', '.')
 		el.src = 'audios_spanish/' + filename;
 		el.play();
 	}
+
+	function rewind15() {
+		var el = document.getElementById('audio');
+		el.currentTime -= 10;
+		el.play();
+	}
+
+	function forward15() {
+		var el = document.getElementById('audio');
+		el.currentTime += 10;
+		el.play();
+	}
+
 </script>
 
 <body style="background-color:azure">
 	<div style="display: flex; flex-direction: row; justify-content: center; padding: 20px;">
-		<div id="player">
-			<audio controls id="audio">
-				<source id="source" src="audios/<?php echo $filenames[0] ?>" type="audio/mpeg">
+		<button style="display: block; height: 40px;" onclick="rewind15()">-10sec</button>
+		<div id="player" style="width: 100%;">
+			<audio controls id="audio" style="width: 100%;">
+				<source id="source" src="audios_spanish/<?php echo $filenames[0] ?>" type="audio/x-wav">
 				Your browser does not support the audio element.
 			</audio>
 		</div>
+		<button style="display: block; height: 40px;" onclick="forward15()">+10sec</button>
 	</div>
 	<div style="display: flex; flex-direction: row; justify-content: center;  flex-wrap: wrap;">
 		<button style="width: 175px; margin: 3px;" onClick="changeAudio('userguide.mp3')">
 			<?php $name = explode(' ', str_replace('.mp3', '', $lesson));
 			echo 'User Guide'; ?>
 		</button>
-		<?php foreach ($filenames as $lesson) { 
-			if ($lesson == 'userguide.mp3') continue;?>
+		<?php foreach ($filenames as $lesson) {
+			if ($lesson == 'userguide.mp3') continue; ?>
 			<button style="width: 175px; margin: 3px;" onClick="changeAudio('<?php echo $lesson ?>')">
 				<?php $name = explode(' ', str_replace('.mp3', '', $lesson));
-				echo 'Spanish ' . $name[1] . ' - Lesson - ' . $name[4]; ?>
+				echo 'Spanish ' . $name[1] . ' - Lesson - ' . str_replace('.new', '', $name[4]); ?>
 			</button>
 		<?php } ?>
 	</div>
